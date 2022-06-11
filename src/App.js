66caecddbb90ca60dfb25861ae852a44ce930096
shiva moeini <shivamoeini2000@gmail.cpm>
2022-06-11 12:46:33 +0430

    cmitchenge

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import DisplayMessages from "./componnet/Mange";
-import Nav from "./componnet/Nav";
+
 function App() {
   return (
     <>
-    <Nav/>
-      <Routes>
-        <Route to="/mang" element={<DisplayMessages />}/>
-      </Routes>
-    
+   
     </>
   );
 }
