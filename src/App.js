08ba39e9ace98f65cf21317c6b2845ab2128c97f
shiva moeini <shivamoeini2000@gmail.cpm>
2022-06-15 13:48:23 +0430

    fix header error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./containers/Header";
 import ProductListing from "./containers/ProductListing";
 import ProductDetail from "./containers/ProductDetail";
@@ -8,11 +8,11 @@ function App() {
     <div className="App">
       <Router>
         <Header />
-        <Switch>
+        <Routes>
           <Route path="/" exact component={ProductListing} />
           <Route path="product/:productId" exact component={ProductDetail} />
           <Route>404 Not Found!</Route>
-        </Switch>
+        </Routes>
       </Router>
     </div>
   );
