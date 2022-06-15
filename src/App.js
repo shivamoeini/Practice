29d6f60253ead3a31a25a71fecc45f323879d4ea
shiva moeini <shivamoeini2000@gmail.cpm>
2022-06-15 17:56:54 +0430

    cmitfetchingdata

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router,  Routes , Route } from "react-router-dom";
 import Header from "./containers/Header";
 import ProductListing from "./containers/ProductListing";
 import ProductDetail from "./containers/ProductDetail";
+import NotFound from "./containers/Notfound";
 function App() {
   return (
-    <div className="App">
+    <div>
       <Router>
         <Header />
         <Routes>
-          <Route path="/" exact component={ProductListing} />
-          <Route path="product/:productId" exact component={ProductDetail} />
-          <Route>404 Not Found!</Route>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </div>
